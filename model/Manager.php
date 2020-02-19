6c4dea588e9eb8cf76010e5854247e80216a769f
mmallet66 <m.mallet66@outlook.fr>
2020-02-19 18:02:46 +0100

    add database connection
    creation of the manager class with function dbConnect()

---
+++ model/Manager.php
@@ -0,0 +1,14 @@
+<?php
+
+class Manager
+{
+
+  protected function dbConnect()
+  {
+
+    $db = new PDO("mysql:host=localhost;dbname=forteroche_blog;charset=utf8", "mathieu", "passe");
+
+    return $db;
+  }
+
+}
