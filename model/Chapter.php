78f99ab3b4480f16c94ce67397b24b9bffd93c34
mmallet66 <m.mallet66@outlook.fr>
2020-02-21 20:13:33 +0100

    Creation of the chapter class

---
+++ model/Chapter.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Class Comment
+ * 
+ * Allows you to create a chapter object
+ */
+class Chapter
+{
+  /**
+   * @var int Chapter identifier in the database
+   */
+  private $_id;
+
+  /**
+   * @var int Number order of the chapter
+   */
+  private $_numberOrder;
+
+  /**
+   * @var string Chapter title, it can contain html tags
+   */
+  private $_title;
+
+  /**
+   * @var html Text of the chapter, it contains html tags
+   */
+  private $_content;
+
+  /**
+   * @var int 0 or 1, serves as a boolean for know if the chapter is published or not
+   */
+  private $_published;
+
+  /**
+   * Hydratation method
+   * 
+   * For each key in an array, it call the setter (if it exists) method for assigned a value to each property.
+   * 
+   * @param array $data
+   */
+  public function hydrate($data) {
+    foreach ($data as $key => $value)
+    {
+      $method = "set" . ucfirst($key);
+
+      if(method_exists($this, $method))
+      {
+        $this->$method($value);
+      }
+    }
+  }
+
+// SETTERS :
+  /**
+   * @param int $id Value assigned to $_id property
+   */
+  public function setId($id) {
+    $this->_id = (int) $id;
+  }
+
+  /**
+   * @param int $number Value assigned to $_number property
+   */
+  public function setNumberOrder($number)
+  {
+    $number = (int) $number;
+
+    if ($number > 0)
+    {
+      $this->_numberOrder = $number;
+    }
+  }
+
+  /**
+   * @param html Value assigned to $_title property
+   */
+  public function setTitle($html)
+  {
+    if (is_string($html))
+    {
+      $this->_title = $html;
+    }
+  }
+
+  /**
+   * @param html Value assigned to $_content property
+   */
+  public function setContent($html)
+  {
+    if (is_string($html))
+    {
+      $this->_content = $html;
+    }
+  }
+
+  /**
+   * @param int Value assigned to $_published property
+   */
+  public function setPublished($state)
+  {
+    $published = (int) $state;
+
+    if ($published === 0 || $published === 1)
+    {
+      $this->_published = $published;
+    }
+  }
+
+
+// GETTERS :
+  public function getId() { return $this->_id; }
+  public function getNumberOrder() { return $this->_numberOrder; }
+  public function getTitle() { return $this->_title; }
+  public function getContent() { return $this->_content; }
+  public function getPublished() { return $this->_published; }
+
+}
