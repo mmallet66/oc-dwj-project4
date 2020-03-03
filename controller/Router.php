a87f54c5300602e0e87d14b2f8db6822496efc14
mmallet66 <m.mallet66@outlook.fr>
2020-03-03 16:14:56 +0100

    refactoring of the router

--- controller/Router.php
+++ controller/Router.php
@@ -3,131 +3,80 @@
 class Router 
 {
   public function traitment(){
-    $frontController = new FrontController();
-    $backController = new BackController();
-    $userController = new UserController();
-
-
     try
     {
-      if(!isset($_GET["admin"]))
+      if (isset($_GET["action"]))
       {
-        if (isset($_GET["page"]))
+        switch($_GET["action"])
         {
-          switch($_GET["page"])
-          {
-            case "read":
-              if(isset($_POST["chapterId"]) && $_POST["chapterId"] > 0)
-              {
-                $frontController->getReadChapter($_POST["chapterId"]);
-              }
-              elseif(isset($_GET["chapterId"]) && $_GET["chapterId"] > 0)
-              {
-                $frontController->getReadChapter($_GET["chapterId"]);
-              }
-              elseif(isset($_GET["report"]))
-              {
-                $frontController->reportComment($_GET["commentId"]);
-              }
-              else{
-                $frontController->getReadPage();
-              }
-              break;
-      
-            case "about":
-              $frontController->getAboutPage();
-              break;
-            
-            case "login":
-              if(isset($_GET["action"]))
-              {
-                if (session_status() == PHP_SESSION_NONE) {
-                  session_start();
-                }
-                switch ( $_GET["action"])
-                {
-                  case "connect":
-                    if($userRole = $userController->checkUser($_POST))
-                    {
-                      $_SESSION["username"] = $_POST["username"];
-                      $_SESSION["role"] = $userRole;
-                      $frontController->getAccueilPage();
-                    }
-                    else
-                    {
-                      $frontController->getLoginPage(true);
-                    }
-                  break;
-      
-                  case "disconnect":
-                    session_unset();
-                    $frontController->getAccueilPage();
-                  break;
-                }
-              }
-              else
-              {
-                $frontController->getLoginPage();
-              }
-              break;
-      
-            case "registration":
-              if(isset($_GET["action"]) && $_GET["action"] === "new")
-              {
-                $userController->addNewUser($_POST);
-              }
-              else
-              {
-                $frontController->getRegistrationPage();
-              }
-              break;
-      
-            default:
-              $frontController->getAccueilPage();
-              break;
-          }
-        }
-        elseif (isset($_GET["action"]) && $_GET["action"] == "makeAComment")
-        {
-          if(!empty($_POST["content"]) && $_GET["chapterId"] > 0)
-          {
-            $frontController->createComment([
-              "author" => $_GET["author"],
-              "content" => $_POST["content"],
-              "chapterId" => $_GET["chapterId"]
-            ]);
-          }
-        }
-        else
-        {
-          $frontController->getAccueilPage();
+          case "about":
+            require "view/front-office/about.php";
+          break;
+
+          case "read-synopsis":
+            $chapterController = new ChapterController();
+            $chapterController->readSynopsis();
+          break;
+
+          case "read-chapter":
+            if(isset($_GET["chapterId"]))
+            {
+              $chapterId = $_GET["chapterId"];
+            }
+            if(isset($_POST["chapterId"]))
+            {
+              $chapterId = $_POST["chapterId"];
+            }
+            $chapterController = new ChapterController();
+            $chapterController->readChapter($chapterId);
+          break;
+
+          case "report-comment":
+            $commentId = $this->getParameter($_GET, "commentId");
+            $commentController = new CommentController();
+            $commentController->reportComment($commentId);
+          break;
+          
+          case "login":
+            require "view/front-office/login.php";
+          break;
+
+          case "connect":
+          break;
+
+          case "disconnect":
+            session_unset();
+          break;
+
+          case "registration":
+            require "view/front-office/registration.php";
+          break;
+
+          case "new-user":
+          break;
+
+          default:
+            throw new Exception("Oups ! Cette page n'existe pas.");
+          break;
         }
       }
       else
       {
-        if (session_status() == PHP_SESSION_NONE) {
-          session_start();
-        }
-        if($backController->accessControl())
-        {
-          switch($_GET["admin"]):
-            case "accueil":
-              $backController->getAccueilPage();
-            break;
-            case "administration":
-            break;
-            case "edition":
-            break;
-            case "moderation":
-            break;
-          endswitch;
-        }
+        require "view/front-office/accueil.php";
       }
     }
     catch(Exception $e)
     {
       $errorMessage = $e->getMessage();
-      $frontController->getErrorPage($errorMessage);
+      require "view/front-office/error.php";
     }
+  }
+
+  private function getParameter($container, $key) {
+    if (isset($container[$key])) {
+        return $container[$key];
+    }
+    else
+        throw new Exception("Paramètre '$key' absent");
   }
 }
