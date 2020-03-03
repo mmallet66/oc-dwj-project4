fb157cac8a9200f5f4295ba7ef9f359f6bc55438
mmallet66 <m.mallet66@outlook.fr>
2020-03-03 16:10:40 +0100

    replace front and back controller by comment and chapter controller

---
+++ controller/ChapterController.php
@@ -0,0 +1,41 @@
+<?php
+
+class ChapterController
+{
+  public $chapterManager;
+  public $chapter;
+
+  public function __construct()
+  {
+    $this->chapterManager = new ChapterManager();
+    $this->chapter = new Chapter();
+  }
+
+  /**
+   * Call the "readSynopsis" view
+   */
+  public function readSynopsis()
+  { $this->chapter->hydrate($this->chapterManager->getChapter(1));
+    $data = $this->chapterManager->getChaptersPublished();
+
+    require "view/front-office/readSynopsis.php";
+  }
+
+  /**
+   * Call the "readChapter" view
+   */
+  public function readChapter(int $chapterId)
+  {
+    $this->chapter->hydrate($this->chapterManager->getChapter($chapterId));
+    if($this->chapter->getId() && $this->chapter->getId() != 1)
+    {
+      $this->commentManager = new CommentManager;
+      $comments = $this->commentManager->getCommentsOfChapter($chapterId);
+      require "view/front-office/readChapter.php";
+    }
+    else
+    {
+      throw new Exception("Une erreur est survenue");
+    }
+  }
+}
