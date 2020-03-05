12e01d8e1a8f76f0b997ae34ccb695a93f3dc8e3
mmallet66 <m.mallet66@outlook.fr>
2020-03-05 14:21:41 +0100

    update controller
    Added post method for new comment

--- controller/CommentController.php
+++ controller/CommentController.php
@@ -11,6 +11,17 @@ class CommentController
     $this->comment = new Comment();
   }
 
+  public function postNewComment (array $commentData)
+  {
+    $this->comment->hydrate($commentData);
+    $affectedLine = $this->commentManager->addComment($this->comment);
+    if(!$affectedLine):
+      throw new Exception("Le commentaire n'a pu être enregistré");
+    endif;
+
+    header("Location: index.php?action=read-chapter&chapterId=" . $this->comment->getChapterId());
+  }
+
   public function reportComment(int $commentId)
   {
     $this->comment->hydrate($this->commentManager->getComment($commentId));
